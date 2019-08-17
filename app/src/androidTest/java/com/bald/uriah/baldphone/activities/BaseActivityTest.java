16135aa73a16151871132b8b16979577aaad3ba0
Uriah Shaul Mandel <uriah.shm@outlook.com>
2019-08-17 17:20:28 +0300

    #39 Added the old tests back

---
+++ app/src/androidTest/java/com/bald/uriah/baldphone/activities/BaseActivityTest.java
@@ -0,0 +1,56 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.bald.uriah.baldphone.activities;
+
+import com.bald.uriah.baldphone.utils.BPrefs;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
+
+public class BaseActivityTest {
+    @Before
+    public void setUp() {
+        BPrefs
+                .get(getInstrumentation().getTargetContext())
+                .edit()
+                .putBoolean(BPrefs.TEST_KEY, true)
+                .putBoolean(BPrefs.VIBRATION_FEEDBACK_KEY, true)
+                .putBoolean(BPrefs.LONG_PRESSES_KEY, true)
+                .putBoolean(BPrefs.LONG_PRESSES_SHORTER_KEY, false)
+                .putBoolean(BPrefs.TOUCH_NOT_HARD_KEY, false)
+                .commit();
+    }
+
+    @After
+    public void after() {
+        BPrefs
+                .get(getInstrumentation().getTargetContext())
+                .edit()
+                .clear()
+                .commit();
+    }
+
+    public void sleep() {
+        getInstrumentation().waitForIdleSync();
+
+    }
+}
