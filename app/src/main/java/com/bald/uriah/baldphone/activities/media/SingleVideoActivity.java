9095eb36a99b12232161a9df5e59e0f4643e24fa
Uriah <uriah.shm@outlook.com>
2020-10-10 14:19:16 +0300

    Fixed Q image deletion

--- app/src/main/java/com/bald/uriah/baldphone/activities/media/SingleVideoActivity.java
+++ app/src/main/java/com/bald/uriah/baldphone/activities/media/SingleVideoActivity.java
@@ -84,7 +84,7 @@ public class SingleVideoActivity extends SingleMediaActivity implements Constant
         }
 
         @Override
-        protected void delete(Activity activity, Cursor cursor) {
+        protected void delete(Activity activity) {
             final int id = cursor.getInt(cursor.getColumnIndex(MediaStore.MediaColumns._ID));
             final Uri deleteUri = ContentUris.withAppendedId(MediaStore.Video.Media.EXTERNAL_CONTENT_URI, id);
             activity.getContentResolver().delete(
@@ -92,6 +92,19 @@ public class SingleVideoActivity extends SingleMediaActivity implements Constant
                     MediaStore.MediaColumns.DATA + "=?",
                     new String[]{getPath(deleteUri)});
 
+        }
+
+        @Override
+        protected void deletePost29(Activity activity) throws SecurityException {
+            final int id =
+                    cursor.getInt(cursor.getColumnIndex(MediaStore.MediaColumns._ID));
+            final Uri deleteUri =
+                    ContentUris.withAppendedId(MediaStore.Video.Media.EXTERNAL_CONTENT_URI, id);
+
+            activity.getContentResolver().delete(
+                    deleteUri,
+                    null,
+                    null);
         }
 
         private String getPath(Uri uri) {
